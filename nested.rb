083248263323ae3ff7fb0cacf4fc5fea03cb4737
Valeria Ragonese <ribesnero80@gmail.com>
2018-09-29 15:52:32 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -70,7 +70,7 @@ def adding_matz
 # add the following information to the top level of programmer_hash
 
 
-	programmer_hash = 
+	programmer_hash = {
 	:yukihiro_matsumoto => {
   :known_for => "Ruby",
     :languages => ["LISP, C"]
