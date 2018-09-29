5632a9f31344e4476ffb7e09572b767d03851c17
Valeria Ragonese <ribesnero80@gmail.com>
2018-09-29 15:53:15 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -71,13 +71,12 @@ def adding_matz
 
 
 	programmer_hash = {
-	:yukihiro_matsumoto => {
-  :known_for => "Ruby",
-    :languages => ["LISP, C"]
-}
+	      :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP, C"]
+        },
 
- 		{
-        :grace_hopper => {
+ 		{ :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
